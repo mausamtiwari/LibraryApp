037e96c7e68c0443dbc8197c8d38be318bf4c7d0
adeepika26@yahoo.in <adeepika26@yahoo.in>
2024-02-06 15:39:49 +0100

    Update Deepika

--- src/main/java/be/intecbrussel/MainLibraryApp.java
+++ src/main/java/be/intecbrussel/MainLibraryApp.java
@@ -1,8 +1,24 @@
 package be.intecbrussel;
 
+import be.intecbrussel.model.book.Book;
+import be.intecbrussel.model.book.ChildrenBooks;
+
 public class MainLibraryApp {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Alexendria.");
+        System.out.println();
+        System.out.println("Sign in");
+
+
+
+
+        Book childrenBook = new ChildrenBooks(9415, "The Lion Inside", "Rachel Bright", 2016, true);
+        .addAnimal(Romy_Dog4);
+
+
+
 
 
     }
