82829e636ecae8b77c47e1155a798d7a840ec22b
adeepika26@yahoo.in <adeepika26@yahoo.in>
2024-02-07 12:34:35 +0100

    updated by Deepika

--- src/main/java/be/intecbrussel/MainLibraryApp.java
+++ src/main/java/be/intecbrussel/MainLibraryApp.java
@@ -2,36 +2,113 @@ package be.intecbrussel;
 
 import be.intecbrussel.model.book.Book;
 import be.intecbrussel.model.book.ChildrenBooks;
+import be.intecbrussel.model.book.FindType;
 import be.intecbrussel.repository.BookRepository;
 import be.intecbrussel.service.BookService;
+
+import java.util.Optional;
+import java.util.Scanner;
 
 public class MainLibraryApp {
 
 
     public static void main(String[] args) {
+        char ans = 'y';
+        Scanner myScanner = new Scanner(System.in);
         BookService bookService = new BookService(new BookRepository());
+        FindType findType;
         System.out.println("Welcome to Alexendria.");
-        System.out.println();
-        System.out.println("Sign in");
-
-
-
-        // Method add book
-        try
+        System.out.println("Enter your status admin/user");
+        String status = myScanner.nextLine().toLowerCase();
+        do
         {
-            Book childrenBook = new ChildrenBooks(0, "The Lion Inside", "Rachel Bright", 2016, true);
-            bookService.addBook(childrenBook);
+            System.out.println("Kindly choose the method you want to perform");
+            switch (status) {
+                case "admin": {
+                    System.out.println("1.Add Book");
+                    System.out.println("2.Find Book");
+                    System.out.println("3.Remove Book");
+                    System.out.println("4.Sort Book");
+                    System.out.println("5.update Book Record");
+                    System.out.println("6.Count Book");
+                    System.out.println("7.Issue Book");
+                    System.out.println("8.Renew Book");
+                    System.out.println("9.Return Book");
+                    System.out.println("9.Browse Books");
+                    System.out.println("10. Add Member");
+                    System.out.println("11.Remove Member");
+                    System.out.println("12.Search Member");
+                    System.out.println("13. Update member info");
+                    System.out.println("14.List Member");
+                    System.out.println("15. Send warning");
+                    System.out.println("16.Exit");
+                    break;
+                }
+                case "member": {
+                    System.out.println("17.Find Book");
+                    System.out.println("18.Browse member info");
+                    System.out.println("19.Browse Book");
+                    System.out.println("20.Reserve Book");
+                    System.out.println("21.Exit");
+                    break;
+                }
+                default: {
+                    System.out.println("Enter valid choice");
+                }
+
+            }
+
+            System.out.println("Kindly Enter your choice");
+            int choice = myScanner.nextInt();
+            switch (choice) {
+                case 1: {
+                    try {
+                        Book childrenBook = new ChildrenBooks(0, "The Lion Inside", "Rachel Bright", 2016, true);
+                        bookService.addBook(childrenBook);
+                    } catch (Exception e) {
+                        System.out.println("Book info can not be null");
+                    }
+                    break;
+
+                }
+                case 2: {
+                    System.out.println("How do you want to find a book Book Title/ Book Author/Book No");
+                    System.out.println("Enter your choice");
+                    System.out.println("1. Book Title");
+                    System.out.println("2. Book Author");
+                    System.out.println("3. Book No");
+                    choice = myScanner.nextInt();
+                    switch (choice) {
+                        case 1: {
+                            findType = FindType.TITLE;
+                            bookService.findTypeControl(findType);
+                            break;
+                        }
+                        case 2: {
+                            findType = FindType.AUTHOR;
+                            bookService.findTypeControl(findType);
+                            break;
+                        }
+                        case 3: {
+                            findType = FindType.ID;
+                            bookService.findTypeControl(findType);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Invalid choice");
+                            break;
+
+                        }
+                    }
+
+
+                }
+
+
+            }
+            System.out.println("Do you want to continue again (y/n)");
+             ans = myScanner.next().charAt(0);
         }
-        catch (Exception e)
-        {
-            System.out.println("Book info can not be null");
-        }
-
-
-
-
-
-
-
+        while (ans == 'y'|| ans == 'Y');
     }
 }
--- src/main/java/be/intecbrussel/service/BookService.java
+++ src/main/java/be/intecbrussel/service/BookService.java
@@ -1,24 +1,30 @@
 package be.intecbrussel.service;
 
 import be.intecbrussel.model.book.Book;
+import be.intecbrussel.model.book.FindType;
 import be.intecbrussel.repository.BookRepository;
 
-import java.util.Comparator;
-import java.util.List;
+import java.net.IDN;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookService {
     private int bookId;
-
+    Scanner  myScanner = new Scanner(System.in);
     private BookRepository bookRepository;
 
-    public BookService(BookRepository bookRepository){
+
+    public BookService(BookRepository bookRepository)
+    {
         this.bookRepository = bookRepository;
 
     }
+
     public void addBook(Book book) throws Exception
     {
-
         List<Book> bookList = bookRepository.getBookList();
+
+        //List<Book> bookList = bookRepository.getBookList();
 
         if (book != null && isValidBook(book))
         {
@@ -36,15 +42,95 @@ public class BookService {
         }
 
     }
-        private boolean isValidBook(Book book) {
+        private boolean isValidBook(Book book)
+        {
         // Example: Check if essential information is not null
         return  book.getBookIDNO() != 0 &&
                 book.getBookAuthor() != null &&
                 book.getBookTitle()!= null && book.bookInLibrary != null && book.getBookPublishYear() > 0 ;
-    }
+        }
 
 
+
+    // method to control type of finding book
+    public void findTypeControl(FindType findType) {
+        switch (findType) {
+            case ID: {
+                System.out.println("Enter Book ID");
+                int bookId = myScanner.nextInt();
+                Optional<Book> foundBookByBookId = findBookByBookId(bookId);
+
+                if (foundBookByBookId.isPresent()) {
+
+                    System.out.println(foundBookByBookId.get());
+                } else {
+                    System.out.println("Book not found with given ID: ");
+                }
+                break;
+
+            }
+
+            case TITLE: {
+                System.out.println("Enter Book Title");
+                String bookTitle = myScanner.nextLine();
+                List<Book> foundBookByBookTitle = findBookByBookTitle(bookTitle);
+
+                if (!foundBookByBookTitle.isEmpty()) {
+                    System.out.println("Books by " + bookTitle + ":");
+                    foundBookByBookTitle.forEach(System.out::println);
+
+
+                } else {
+                    System.out.println("No books found for given: " + bookTitle);
+                }
+                break;
+            }
+
+
+            case AUTHOR: {
+                System.out.println("Enter Book Author");
+                String bookAuthor = myScanner.nextLine();
+                List<Book> foundBookByBookAuthor = findBookByBookAuthor(bookAuthor);
+
+                if (!foundBookByBookAuthor.isEmpty()) {
+                    System.out.println("Books by " + bookAuthor + ":");
+                    foundBookByBookAuthor.forEach(System.out::println);
+                } else {
+                    System.out.println("No books found for author: " + bookAuthor);
+                }
+                break;
+            }
+        }
     }
+
+    public Optional<Book> findBookByBookId(int bookId)
+    {
+        List<Book> bookList = bookRepository.getBookList();
+            return bookList.stream()
+                    .filter(book -> book.getBookIDNO()==bookId)
+                    .findFirst();
+
+    }
+
+    public List<Book> findBookByBookTitle(String bookTitle)
+    {
+        List<Book> bookList = bookRepository.getBookList();
+        return bookList.stream()
+                .filter(book -> book.getBookTitle().equalsIgnoreCase(bookTitle))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Book> findBookByBookAuthor(String bookAuthor)
+    {
+        List<Book> bookList = bookRepository.getBookList();
+        return bookList.stream()
+                .filter(book -> book.getBookAuthor().equalsIgnoreCase(bookAuthor))
+                .collect(Collectors.toList());
+
+    }
+
+}
 
 
 
